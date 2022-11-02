394f1d58b32fb6d4714946fafe03c6ff61f515cd
SarawinT <contact.sarawin@gmail.com>
2022-11-02 21:30:04 +0700

    Add load username from JSON config file.

--- moneytracker_app/lib/main.dart
+++ moneytracker_app/lib/main.dart
@@ -1,13 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:moneytracker_app/pages/homepage.dart';
+import 'package:moneytracker_app/pages/loading_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String username = "";
+
+  void _loadUsername() async {
+    final String response =
+        await rootBundle.loadString('assets/config/config.json');
+    setState(() {
+      username = jsonDecode(response)['Username'];
+    });
+  }
+
+  @override
+  void initState() {
+    _loadUsername();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +45,15 @@ class MyApp extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      home: Homepage(username: "MeisterAP",),
+      home: Stack(
+        children: [
+          if (username.isNotEmpty)
+            Homepage(
+              username: username,
+            ),
+          if (username.isEmpty) const LoadingPage(),
+        ],
+      ),
     );
   }
 }
